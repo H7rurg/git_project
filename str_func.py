d27f583366b947b306fbb24018947b7671a41696
Maksim_Bakhtin <xegxbr@yandex.ru>
2023-11-15 22:33:38 +0300

    func upper and func_upp_first

--- str_func.py
+++ str_func.py
@@ -1,2 +1,15 @@
 def func_upp(s):
+    """
+    12345
+    """
     print(s.upper())
+
+
+def func_upp_first(d):
+    """
+    45678
+    """
+    print(d.title())
+
+
+func_upp_first('hello world')
